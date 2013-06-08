2b6809d8127d2f78a408f4aaa1bbd5de6352cf80
Percy Liang <percyliang@gmail.com>
2013-06-08 11:23:53 -0700

    small changes to ImmutableList interface

--- src/main/java/fig/basic/ImmutableList.java
+++ src/main/java/fig/basic/ImmutableList.java
@@ -3,22 +3,28 @@ package fig.basic;
 import java.util.*;
 
 /**
- * An immutable single-linked list, which allows for sharing tails.
+ * An immutable single-linked list, which allows for sharing suffixes.
  */
 public class ImmutableList<T> {
   public T value;
   public ImmutableList<T> next;
+  private int size;
 
-  private ImmutableList(T value, ImmutableList<T> next) {
+  private ImmutableList(T value, ImmutableList<T> next, int size) {
     this.value = value;
     this.next = next;
+    this.size = size;
   }
 
-  public boolean isEmpty() { return this == empty; }
+  public boolean isEmpty() { return this == emptyList; }
 
-  public int size() { return isEmpty() ? 0 : 1 + next.size(); }
+  public int size() { return size; }
 
-  public ImmutableList<T> prepend(T value) { return new ImmutableList(value, this); }
+  // Returns a list with |value| prepended to |this|.
+  // Main operation to construct lists.
+  public ImmutableList<T> prepend(T value) { return new ImmutableList(value, this, 1 + size); }
 
-  public static ImmutableList empty = new ImmutableList(null, null);
+  // Create lists
+  public static ImmutableList emptyList = new ImmutableList(null, null, 0);
+  public static <T> ImmutableList<T> singletonList(T value) { return new ImmutableList<T>(value, emptyList, 1); }
 }
